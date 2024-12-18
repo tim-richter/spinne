41fb06cac2da9932be35d915a132e00d9388a13e
tim <tim>
2024-12-18 12:44:15 +0100

    remove stuff

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,16 +1,12 @@
 mod analyze;
-mod config;
 mod graph;
 mod package_json;
 mod parse;
 mod resolve;
 mod traverse;
-mod ts_config_reader;
 mod util;
 
-pub use config::Config;
 pub use graph::ComponentGraph;
 pub use package_json::PackageJson;
 pub use resolve::resolve_file_path;
 pub use traverse::Project;
-pub use ts_config_reader::TsConfigReader;
