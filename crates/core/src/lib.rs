c5a78deca9df928fe3098b1e1cf09c4913566d87
tim <tim>
2025-01-11 11:33:51 +0100

    refactor: remove resolve module and integrate functionality into ProjectResolver
    - Deleted the `resolve.rs` module, which contained the `resolve_file_path` function.
    - Integrated the file path resolution logic directly into the `ProjectResolver` struct in `resolver.rs`.
    - Updated the public API to use `ProjectResolver` for resolving file paths, enhancing encapsulation and reducing module complexity.
    - Added tests for the new resolution logic within the `ProjectResolver` to ensure functionality remains intact.
    
    This change streamlines the codebase by consolidating related functionality and improving the organization of the resolver logic.

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -2,11 +2,10 @@ mod analyze;
 mod graph;
 mod package_json;
 mod parse;
-mod resolve;
 mod traverse;
 mod util;
 
 pub use graph::ComponentGraph;
 pub use package_json::PackageJson;
-pub use resolve::resolve_file_path;
 pub use traverse::Project;
+pub use traverse::ProjectResolver;
--- crates/core/src/traverse/resolver.rs
+++ crates/core/src/traverse/resolver.rs
@@ -1,8 +1,6 @@
 use std::path::PathBuf;
 
-use oxc_resolver::Resolution;
-
-use crate::resolve_file_path;
+use oxc_resolver::{Resolution, ResolveOptions, Resolver, TsconfigOptions, TsconfigReferences};
 
 pub struct ProjectResolver {
     tsconfig_path: Option<PathBuf>,
@@ -13,7 +11,156 @@ impl ProjectResolver {
         Self { tsconfig_path }
     }
 
+    /// resolve a relative file path to an absolute file path
+    ///
+    /// dir: is the directory of the file that has the import statement
+    /// specifier: is the relative file path that the import statement is importing
+    /// tsconfig: is the path to the tsconfig.json file that contains the tsconfig options and tsconfigPaths
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use std::path::PathBuf;
+    /// use spinne_core::ProjectResolver;
+    ///
+    /// let dir = PathBuf::from("/Users/tim/projects/spinne/src/index.ts");
+    /// let resolver = ProjectResolver::new(None);
+    /// let resolution = resolver.resolve(&dir, "./components/Button");
+    /// ```
     pub fn resolve(&self, dir: &PathBuf, specifier: &str) -> Result<Resolution, String> {
-        resolve_file_path(dir, specifier, self.tsconfig_path.as_ref())
+        let options = ResolveOptions {
+            tsconfig: self.tsconfig_path.as_ref().map(|tsconfig| TsconfigOptions {
+                config_file: tsconfig.to_path_buf(),
+                references: TsconfigReferences::Auto,
+            }),
+            condition_names: vec!["node".to_string(), "import".to_string()],
+            extensions: vec![
+                ".ts".to_string(),
+                ".tsx".to_string(),
+                ".js".to_string(),
+                ".jsx".to_string(),
+            ],
+            extension_alias: vec![(
+                ".js".to_string(),
+                vec![".ts".to_string(), ".js".to_string()],
+            )],
+            ..ResolveOptions::default()
+        };
+
+        match Resolver::new(options).resolve(dir, &specifier) {
+            Ok(resolved_path) => Ok(resolved_path),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use tempfile::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn test_resolve_file_path() {
+        let temp_dir = TempDir::new().unwrap();
+        let root = temp_dir.path();
+
+        let src_dir = root.join("src");
+        let components_dir = src_dir.join("components");
+        fs::create_dir_all(&src_dir).unwrap();
+        fs::create_dir_all(&components_dir).unwrap();
+
+        let button_file = components_dir.join("Button.tsx");
+        let index_file = components_dir.join("index.ts");
+        fs::write(
+            &button_file,
+            "export function Button() { return <div>Button</div>; }",
+        )
+        .unwrap();
+        fs::write(&index_file, "import { Button } from './Button';").unwrap();
+
+        let specifier = "./components/Button";
+        let resolver = ProjectResolver::new(None);
+        let resolution = resolver.resolve(&src_dir, &specifier);
+
+        assert!(resolution.is_ok());
+        assert_eq!(
+            resolution.unwrap().path(),
+            components_dir.join("Button.tsx")
+        );
+    }
+
+    #[test]
+    fn test_resolve_file_path_with_tsconfig_paths() {
+        let temp_dir = TempDir::new().unwrap();
+        let root = temp_dir.path();
+
+        let src_dir = root.join("src");
+        let components_dir = src_dir.join("components");
+        fs::create_dir_all(&src_dir).unwrap();
+        fs::create_dir_all(&components_dir).unwrap();
+
+        let button_file = components_dir.join("Button.tsx");
+        let index_file = components_dir.join("index.ts");
+        fs::write(
+            &button_file,
+            "export function Button() { return <div>Button</div>; }",
+        )
+        .unwrap();
+        fs::write(&index_file, "import { Button } from './Button';").unwrap();
+
+        let tsconfig_path = root.join("tsconfig.json");
+        fs::write(
+            &tsconfig_path,
+            r#"{
+        "compilerOptions": {
+          "baseUrl": ".",
+          "paths": {
+            "@components/*": ["./src/components/*"]
+          }
+        }
+      }"#,
+        )
+        .unwrap();
+        let specifier = "@components/Button";
+        let resolver = ProjectResolver::new(Some(tsconfig_path));
+        let resolution = resolver.resolve(&src_dir, &specifier);
+
+        assert!(resolution.is_ok());
+        assert_eq!(
+            resolution.unwrap().path(),
+            components_dir.join("Button.tsx")
+        );
+    }
+
+    #[test]
+    fn test_resolve_file_path_with_node_modules() {
+        let temp_dir = TempDir::new().unwrap();
+        let root = temp_dir.path();
+
+        let src_dir = root.join("src");
+        let node_modules_dir = root.join("node_modules");
+        fs::create_dir_all(&src_dir).unwrap();
+        fs::create_dir_all(&node_modules_dir).unwrap();
+
+        let framer_motion_dir = node_modules_dir.join("framer-motion");
+        fs::create_dir_all(&framer_motion_dir).unwrap();
+        fs::write(
+            &framer_motion_dir.join("index.js"),
+            "module.exports = { motion: () => <div>Framer Motion</div> };",
+        )
+        .unwrap();
+
+        let specifier = "framer-motion";
+        let resolver = ProjectResolver::new(None);
+        let resolution = resolver.resolve(&src_dir, &specifier);
+
+        assert!(resolution.is_ok());
+        assert_eq!(
+            resolution.unwrap().path(),
+            node_modules_dir.join("framer-motion").join("index.js")
+        );
     }
 }
